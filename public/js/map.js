00eaeca708822330d9244453b3501b38c767505f
Aldo Lopez <aldo@lamediadl.com>
2022-08-28 11:35:39 -0500

    Set distance in map

--- public/js/map.js
+++ public/js/map.js
@@ -1,76 +1,76 @@
-let  map;
+let map;
 let marker;
 let infoWindow;
 const markers = [
-    {lat:-12.092513 ,lng: -77.024200,quantity:60,color:'green'},
-    {lat:-12.0925502,lng: -77.020291,quantity:10,color:'yellow'},
-    {lat:-12.0972407 ,lng: -77.0224126,quantity:40,color:'red'},
+    { lat: -12.092513, lng: -77.024200, quantity: 60, color: 'green' },
+    { lat: -12.0925502, lng: -77.020291, quantity: 10, color: 'yellow' },
+    { lat: -12.0972407, lng: -77.0224126, quantity: 40, color: 'red' },
 ];
-let  directionsService;
-let  directionsDisplay;
+let directionsService;
+let directionsDisplay;
 
-function iniciarMap(){
-        var coord = {lat:-12.093913 ,lng: -77.021239};
-        map = new google.maps.Map(document.getElementById('map'),{
-            zoom: 10,
-            center: coord
-        });
-        // marker = new google.maps.Marker({
-        //     position: coord,
-        //     icon:'./images/person-location-2.svg',
-        //     map: map,
-        // });
-        const searchMap = document.getElementById('search-map');
-        const autocomplete = new google.maps.places.Autocomplete(searchMap);
-        autocomplete.addListener("place_changed",()=>{
-            const place = autocomplete.getPlace();
-            const {geometry:{viewport,location}} = place;
-            map.setCenter(location);
-            map.setZoom(16);
-            addInfoMap(location);
-        })
+function iniciarMap() {
+    var coord = { lat: -12.093913, lng: -77.021239 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: coord
+    });
+    // marker = new google.maps.Marker({
+    //     position: coord,
+    //     icon:'./images/person-location-2.svg',
+    //     map: map,
+    // });
+    const searchMap = document.getElementById('search-map');
+    const autocomplete = new google.maps.places.Autocomplete(searchMap);
+    autocomplete.addListener("place_changed", () => {
+        const place = autocomplete.getPlace();
+        const { geometry: { viewport, location } } = place;
+        map.setCenter(location);
+        map.setZoom(16);
+        addInfoMap(location);
+    })
 }
 var code_postal;
-async function searchPostalCode(coord){
+async function searchPostalCode(coord) {
     geocoder = new google.maps.Geocoder();
-    
-    let data ={
-        address:'Dirección no encontrada.',
+
+    let data = {
+        address: 'Dirección no encontrada.',
     }
-    const {results} =await geocoder.geocode({'latLng': coord}, ()=>{});
+    const { results } = await geocoder.geocode({ 'latLng': coord }, () => { });
     if (results[0]) {
-        data.address =results[0].formatted_address;
+        data.address = results[0].formatted_address;
     }
     return data;
 }
 setCurrentPositionMap();
-function setCurrentPositionMap(){
+function setCurrentPositionMap() {
     let loader = document.getElementById("loader-custom");
-        loader.classList.remove("d-none");
-        if ("geolocation" in navigator){
-        navigator.geolocation.getCurrentPosition(function(position){ 
+    loader.classList.remove("d-none");
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             var currentLatitude = position.coords.latitude;
             var currentLongitude = position.coords.longitude;
             var currentLocation = { lat: currentLatitude, lng: currentLongitude };
             marker_current = new google.maps.Marker({
                 position: currentLocation,
-                icon:'./images/person-location-2.svg',
+                icon: './images/person-location-2.svg',
                 map: map,
             });
-            markers.forEach((marker,index) => {
+            markers.forEach((marker, index) => {
                 let location_marker = {
-                    lat:marker.lat,
-                    lng:marker.lng
+                    lat: marker.lat,
+                    lng: marker.lng
                 };
-                const marker_google =new google.maps.Marker({
+                const marker_google = new google.maps.Marker({
                     position: location_marker,
-                    icon:`../images/marker-${marker.color}.svg`,
+                    icon: `../images/marker-${marker.color}.svg`,
                     map: map,
                 })
-                if(index==0){
-                    setRoute(marker,marker_google,marker_current);
+                if (index == 0) {
+                    setRoute(marker, marker_google, marker_current,location_marker,currentLocation);
                 }
-                let fn = infoFn(location_marker,marker,marker_google,marker_current);
+                let fn = infoFn(location_marker, marker, marker_google, marker_current,currentLocation);
                 google.maps.event.addListener(marker_google, 'click', fn);
             });
 
@@ -80,40 +80,80 @@ function setCurrentPositionMap(){
         loader.classList.add("d-none");
     }
 }
-let infoFn = function (currentLocation,marker,marker_google,marker_current) {
+let infoFn = function (location_marker, marker, marker_google, marker_current,currentLocation) {
     return async function (e) {
-        if(infoWindow){
+        if (infoWindow) {
             infoWindow.close();
         }
-        const {address} =await searchPostalCode(currentLocation);
-        var content = `<div>Dirección: ${address}</div><br><div>Afluencia: ${marker.quantity} personas</div>`;
-        infoWindow = new google.maps.InfoWindow({map: map, content: content});
-        infoWindow.setPosition(currentLocation);
-        setRoute(marker,marker_google,marker_current);
+        const { address } = await searchPostalCode(location_marker);
+        const  {distance,duration} = await calculateTimeDistance(currentLocation,location_marker);
+
+        var content = `<div>Dirección: ${address}</div><br><div>Afluencia: ${marker.quantity} personas</div><br><div>Distancia: ${distance} (${duration} - en carro)</div>`;
+        infoWindow = new google.maps.InfoWindow({ map: map, content: content });
+        infoWindow.setPosition(location_marker);
+        setRoute(marker, marker_google, marker_current);
+        // Calculate distance
         
     }
 };
-function setRoute(marker,marker_google,marker_current){
-    directionsDisplay  = null;
+function setRoute(marker, marker_google, marker_current,location_marker,currentLocation) {
+    // Calculate route 
+    directionsDisplay = null;
     directionsService = null;
 
     directionsService = new google.maps.DirectionsService();
     directionsDisplay = new google.maps.DirectionsRenderer();
 
-    const request = {
+        const request = {
         travelMode: google.maps.TravelMode.DRIVING
     };
-    request.origin = marker_current.getPosition();
+    const position_current = marker_current.getPosition();
+    request.origin = position_current;
     if (!request.waypoints) request.waypoints = [];
     request.waypoints.push({
-        location: {lat:marker.lat,lng:marker.lng},
+        location: { lat: marker.lat, lng: marker.lng },
         stopover: true,
-      });
-      request.destination = marker_google.getPosition();
+    });
+    const position_destination = marker_google.getPosition();
+    request.destination = position_destination;
     directionsDisplay.setMap(map);
-    directionsService.route(request, function(result, status) {
+    directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(result);
+            directionsDisplay.setDirections(result);
         }
-      });
+    });
 }
+async function calculateTimeDistance(currentLocation,location_marker){
+    let origin1 = new google.maps.LatLng(
+        currentLocation.lat,
+        currentLocation.lng
+    );
+    let destinationB = new google.maps.LatLng(
+        location_marker.lat,
+        location_marker.lng
+    )
+    let service = new google.maps.DistanceMatrixService();
+    let response = await service.getDistanceMatrix(
+    {
+        origins: [origin1],
+        destinations: [destinationB],
+        travelMode: 'DRIVING',
+        // transitOptions: TransitOptions,
+        // drivingOptions: DrivingOptions,
+        // unitSystem: UnitSystem,
+        // avoidHighways: Boolean,
+        // avoidTolls: Boolean,
+    }, ()=>{});
+    let origins = response.originAddresses;
+    let distance = '';
+    let duration = '';
+    for (let i = 0; i < origins.length; i++) {
+        let results = response.rows[i].elements;
+        for (let j = 0; j < results.length; j++) {
+            let element = results[j];
+            distance = element.distance.text;
+            duration = element.duration.text;
+        }
+    }
+    return {distance:distance,duration:duration};
+};
